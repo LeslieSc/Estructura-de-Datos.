c084f3860d97dc19c308ef71c143dc9ad97e2a9b
Leslie Sosa <15248@ulsachihuahua.edu.mx>
2025-02-06 10:45:44 -0600

    Se el archivo UserData.java y se realizaron cambios en el archivo EstructuraDeDatos.java

--- EstructuraDeDatos.java
+++ EstructuraDeDatos.java
@@ -13,8 +13,19 @@ public class EstructuraDeDatos {
         //StudentGradesAverage objStudentGradesAverage = new StudentGradesAverage();
         //objStudentGradesAverage.GetGradeAverage();
 
-        ClassmateNameList objClassmateNameList = new ClassmateNameList();
-        objClassmateNameList.GetClassmateTotal();
+       //ClassmateNameList objClassmateNameList = new ClassmateNameList();
+        //objClassmateNameList.GetClassmateTotal();
+
+        //Crear instancia de UserData y pasa aqui el apellido y el nombre
+
+        UserData objUserData = new UserData();
+
+        //se declara la variable fullName del tipo String
+        //se le asigna el valor de retorno de la funcion fullName
+        //se le pasa como parametro el nombre y el apellido
+        
+        String fullName = objUserData.fullName("Leslie","Sosa");
+        System.out.println(fullName);
 
 
     }
---
+++ UserData.java
@@ -0,0 +1,13 @@
+/**
+ * Autor: Leslie Anahi Sosa Corral
+ * Matrícula: 15248
+ * Fecha: 04-Feb-2025
+ */
+
+
+public class UserData {
+    public String fullName(String firstName, String lastName) {
+        return String.format("El nombre completo es: %s %s ", firstName, lastName);
+        //return firstName + " " + lastName; concatenacion
+    }
+}
