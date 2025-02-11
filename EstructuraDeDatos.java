831090fe1ecb4e67bf32f1780ac3c81240d55d6e
Leslie Sosa <15248@ulsachihuahua.edu.mx>
2025-02-11 11:59:01 -0600

    Modifique el archivo StudentGrades.java para crear los metodos calculateFinalGrade y checkFailureByAbsences. Tambien Modifique el archivo EstructuraDeDatos.java.

--- EstructuraDeDatos.java
+++ EstructuraDeDatos.java
@@ -4,29 +4,37 @@
  * Fecha: 04-Feb-2025
  */
 
-
 public class EstructuraDeDatos {
     public static void main(String[] args) {
-        //RandomElementGame objRandomElementGame = new RandomElementGame();
-        //objRandomElementGame.GetRandomElement();
+        // RandomElementGame objRandomElementGame = new RandomElementGame();
+        // objRandomElementGame.GetRandomElement();
 
-        //StudentGradesAverage objStudentGradesAverage = new StudentGradesAverage();
-        //objStudentGradesAverage.GetGradeAverage();
+        // StudentGradesAverage objStudentGradesAverage = new StudentGradesAverage();
+        // objStudentGradesAverage.GetGradeAverage();
 
-       //ClassmateNameList objClassmateNameList = new ClassmateNameList();
-        //objClassmateNameList.GetClassmateTotal();
+        // ClassmateNameList objClassmateNameList = new ClassmateNameList();
+        // objClassmateNameList.GetClassmateTotal();
 
-        //Crear instancia de UserData y pasa aqui el apellido y el nombre
+        // Crear instancia de UserData y pasa aqui el apellido y el nombre
 
-        UserData objUserData = new UserData();
+        // UserData objUserData = new UserData();
 
-        //se declara la variable fullName del tipo String
-        //se le asigna el valor de retorno de la funcion fullName
-        //se le pasa como parametro el nombre y el apellido
-        
-        String fullName = objUserData.fullName("Leslie","Sosa");
-        System.out.println(fullName);
+        // se declara la variable fullName del tipo String
+        // se le asigna el valor de retorno de la funcion fullName
+        // se le pasa como parametro el nombre y el apellido
 
+        // String fullName = objUserData.fullName("Leslie","Sosa");
+        // System.out.println(fullName);
+
+        StudentGrades objStudentGrades = new StudentGrades();
+        int partialGradeResult = objStudentGrades.calculateFirstPartialGrade(85, 90);
+        System.out.println("La calificacion parcial es: " + partialGradeResult);
+
+        int finalGradeResult = objStudentGrades.calculateFinalGrade(80, 75, 90, 85);
+        System.out.println("La calificacion final es: " + finalGradeResult);
+
+        String absencesResult = objStudentGrades.checkFailureByAbsences(64, 50);
+        System.out.println(absencesResult);
 
     }
 }
--- StudentGrades.java
+++ StudentGrades.java
@@ -1,16 +1,25 @@
 public class StudentGrades {
     public int calculateFirstPartialGrade(int examScore, int homeworkScore) {
 
-        double firstPartialGrade = ((examScore * 0.7) + (homeworkScore * 0.3) );
-        return(int)(firstPartialGrade);
-        
+        double firstPartialGrade = ((examScore * 0.7) + (homeworkScore * 0.3));
+        return (int) (firstPartialGrade);
+
     }
-    
-    public int calculateFinalGrade(int firstPartialScore, int secondParitalScore, int thirdPartialScore, int finalExamScore) {
+
+    public int calculateFinalGrade(int firstPartial, int secondPartial, int thirdPartial, int finalExam) {
+
+        double finalGrade = (((firstPartial + secondPartial + thirdPartial) / 3) * 0.5) + (finalExam * 0.5);
+        return (int) (finalGrade);
 
     }
 
     public String checkFailureByAbsences(int totalHours, int totalAbsences) {
 
+        double absencePercentage = ((((double) totalAbsences / (double) totalHours)) * 100);
+        if (absencePercentage < 10) {
+            return "Alumno cumple con las asistencias minimas.";
+        } else {
+            return "Alumno no cumple con las asistencias minimas.";
+        }
     }
 }
