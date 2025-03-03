481fcc786966b0b004126a1f8ce025b3526fab94
Leslie Sosa <15248@ulsachihuahua.edu.mx>
2025-03-03 09:40:45 -0600

    Dividi todo en carpetas dividido en tres parciales, y cree dos documentos nuevos

---
+++ SecondPartial/InsertionSort.java
@@ -0,0 +1,20 @@
+package SecondPartial;
+
+public class InsertionSort {
+    
+    int[] array = {8,6,1,2,5};
+
+    public void sort() {
+        int length = array.length; 
+        for (int i = 1; i < length; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j+1] = key; 
+        }
+    }
+}
